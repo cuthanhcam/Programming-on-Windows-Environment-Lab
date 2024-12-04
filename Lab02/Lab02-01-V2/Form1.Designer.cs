005808dd8b58483daed81fcf423949d889d4fb4f
cuthanhcam <cuthanhcam04@gmail.com>
2024-12-04 09:10:47 +0700

    Update Lab02

---
+++ Lab02/Lab02-01-V2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab02_01_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(87, 54);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(473, 98);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(87, 213);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 51);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(186, 213);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 52);
+            this.btnSub.TabIndex = 2;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(287, 211);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 53);
+            this.btnMul.TabIndex = 3;
+            this.btnMul.Text = "X";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(388, 211);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 53);
+            this.btnDiv.TabIndex = 4;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(485, 211);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(75, 53);
+            this.btnEqual.TabIndex = 5;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 334);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Lab02-01-V2 - Cù Thanh Cầm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnEqual;
+    }
+}
+
---
+++ Lab02/Lab02-01-V2/Form1.cs
@@ -0,0 +1,135 @@
+﻿// 2280600285 - Cu Thanh Cam - https://github.com/cuthanhcam
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Lab02_01_V2
+{
+    public partial class Form1 : Form
+    {
+        private List<string> operations = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNumberAndOperator(string operatorSymbol)
+        {
+            if (string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                MessageBox.Show("Vui lòng nhập số trước khi chọn toán tử!");
+                return;
+            }
+
+            operations.Add(txtInput.Text);
+            operations.Add(operatorSymbol);
+
+            txtInput.Clear();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddNumberAndOperator("+");
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            AddNumberAndOperator("-");
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            AddNumberAndOperator("*");
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            AddNumberAndOperator("/");
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                MessageBox.Show("Vui lòng nhập số trước khi nhấn '='!");
+                return;
+            }
+
+            operations.Add(txtInput.Text);
+
+            try
+            {
+                double result = CalculateResult();
+
+                txtInput.Text = string.Join(" ", operations) + " = " + result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message);
+            }
+
+            operations.Clear();
+        }
+
+        private double CalculateResult()
+        {
+            List<string> tempOps = new List<string>(operations);
+
+            if (tempOps.Count < 3 || tempOps.Count % 2 == 0)
+                throw new InvalidOperationException("Phép tính không hợp lệ!");
+
+            for (int i = 0; i < tempOps.Count; i++)
+            {
+                if (tempOps[i] == "*" || tempOps[i] == "/")
+                {
+                    double left = double.Parse(tempOps[i - 1]);
+                    double right = double.Parse(tempOps[i + 1]);
+
+                    if (tempOps[i] == "/" && right == 0)
+                        throw new DivideByZeroException("Không thể chia cho 0!");
+
+                    double intermediateResult = tempOps[i] == "*" ? left * right : left / right;
+
+                    tempOps[i - 1] = intermediateResult.ToString();
+                    tempOps.RemoveAt(i);
+                    tempOps.RemoveAt(i);
+                    i--;
+                }
+            }
+
+
+            double result = double.Parse(tempOps[0]);
+            for (int i = 1; i < tempOps.Count; i += 2)
+            {
+                string op = tempOps[i];
+                double nextValue = double.Parse(tempOps[i + 1]);
+
+                result = op == "+" ? result + nextValue : result - nextValue;
+            }
+
+            return result;
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Bạn có muốn thoát", "Câu hỏi thoát",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
